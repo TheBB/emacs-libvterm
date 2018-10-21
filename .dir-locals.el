bf3973208dbce51f164ca247a40f423049d80d76
Eivind Fonn <evfonn@gmail.com>
2018-10-21 17:14:48 +0200

    Add indent level to .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,8 @@
-((nil . ((projectile-project-compilation-cmd . "mkdir -p build; cd build; cmake ..; make")
-         (projectile-project-run-cmd . "mkdir -p build; cd build; cmake ..; make run")
-         )))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (projectile-project-compilation-cmd . "mkdir -p build; cd build; cmake ..; make")
+  (projectile-project-run-cmd . "mkdir -p build; cd build; cmake ..; make run"))
+ (c-mode
+  (c-basic-offset . 2)))
